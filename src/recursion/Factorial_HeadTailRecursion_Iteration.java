0ab4d93089771a8bb757625a807ddfde6ecfee51
Vinay Chaudhary <vinay.chaudhary@sunlife.com>
2021-11-20 12:04:23 +0530

    factorial iteration updated

--- src/recursion/Factorial_HeadTailRecursion_Iteration.java
+++ src/recursion/Factorial_HeadTailRecursion_Iteration.java
@@ -18,8 +18,17 @@ public class Factorial_HeadTailRecursion_Iteration {
 		
 	}
 	
+	public int factorialIteration(int n) {
+		int result=1;
+		for (int i=1;i<=n;i++) {
+			result = i*result;
+		}
+		return result;
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("Factorial Head Recurion : "+ new Factorial_HeadTailRecursion_Iteration().fatorialHead(5));
-		System.out.println("Factorial Tail Recurion : "+ new Factorial_HeadTailRecursion_Iteration().fatorialTail(5,1));
+		//System.out.println("Factorial Head Recurion : "+ new Factorial_HeadTailRecursion_Iteration().fatorialHead(5));
+		//System.out.println("Factorial Tail Recurion : "+ new Factorial_HeadTailRecursion_Iteration().fatorialTail(5,1));
+		System.out.println("Factorial Iteration  : "+ new Factorial_HeadTailRecursion_Iteration().factorialIteration(5));
 	}
 }
