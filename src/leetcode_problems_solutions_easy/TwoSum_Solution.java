b7fa4f6ce674190c4aade33e44a17eab05d14e5a
Vinay Chaudhary <vcteotia@gmail.com>
2022-02-15 16:33:55 +0530

    two sum optimal slution

--- src/leetcode_problems_solutions_easy/TwoSum_Solution.java
+++ src/leetcode_problems_solutions_easy/TwoSum_Solution.java
@@ -1,19 +1,42 @@
 package leetcode_problems_solutions_easy;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum_Solution {
 
 	public static void main(String[] args) {
-		//TestCase1  nums = [2,7,11,15], target = 9
-		//TestCase2 [3,2,4] 6
-		int nums[] = new int[] {3,2,4}; int target=6;
-		
+		// TestCase1 nums = [2,7,11,15], target = 9
+		// TestCase2 [3,2,4] 6
+		int nums[] = new int[] { 3, 2, 4 };
+		int target = 6;
+
 		int resultArray[] = twoSumSolution1(nums, target);
-		System.out.println("Indexes are : "+resultArray[0] +" , "+resultArray[1]);
-		
+		System.out.println("Indexes are : " + resultArray[0] + " , " + resultArray[1]);
+
 	}
-	
+
+	//Time Complexity = O(n) and Space Comlexity = O(n) as we using HashMap to store approx n elements.
+	//Runtime: 3 ms, faster than 79.66% of Java online submissions for Two Sum.
+	//Memory Usage: 43.1 MB, less than 29.93% of Java online submissions for Two Sum.
+	public int[] twoSumSolution2(int[] nums, int target) {
+		Map futureMap = new HashMap<Integer,Integer>();
+		int result[] = new int[2];
+		for (int i = 0; i < nums.length; i++) {
+
+			int ntf = target - nums[i];
+
+			if (futureMap.get(ntf) != null) {
+				result[0] = (Integer) futureMap.get(ntf);
+				result[1] = i;
+			} else {
+				futureMap.put(nums[i], i);
+			}
+
+		}
+		return result;
+	}
 
 	// Time Complexity = O(nlogn + n + n) which becomes O(nlogn)
 	public static int[] twoSumSolution1(int[] nums, int target) {
@@ -30,7 +53,7 @@ public class TwoSum_Solution {
 				pointer1++;
 			}
 		}
-		//System.out.println(pointer1 + " --  " + pointer2);
+		// System.out.println(pointer1 + " -- " + pointer2);
 		int aa[] = new int[] { -1, -1 };
 		for (int i = 0; i < numTemp.length; i++) { // O(n)
 			if (nums[pointer1] == numTemp[i] && aa[0] == -1) {
@@ -42,6 +65,5 @@ public class TwoSum_Solution {
 		}
 		return aa;
 	}
-
 
 }
